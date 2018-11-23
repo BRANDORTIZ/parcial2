91467450132178a6b6386a3aa995a4a6b675387d
Ricardo Brand Ortiz <ricardobrand9007@gmail.com>
2018-11-23 07:43:41 -0500

    Commit 2

--- parcial2.sql
+++ parcial2.sql
@@ -1,1 +1,19 @@
 -- Parcial2
+
+CREATE OR REPLACE FUNCTION CALCULAR_CAJAS_NECESARIAS (
+    items_number      NUMBER,
+    big_box_av        NUMBER,
+    little_box_av     NUMBER    
+) RETURN NUMBER IS
+    boxes   NUMBER;
+    
+BEGIN
+    IF (MOD(items_number,5) > 0) THEN
+        IF (big_box_av > 0) THEN 
+            boxes := ROUND((items_number / big_box_av), 0) +  MOD(items_number,5);
+        END IF;
+        
+    END IF;
+
+    RETURN boxes;
+END CALCULAR_CAJAS_NECESARIAS;
